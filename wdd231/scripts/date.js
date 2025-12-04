b7f55c419fd39b715ae76894f9bb0941016b6625
joshwagstaff04 <joshuawagstaff04@gmail.com>
2025-12-04 12:40:41 -0700

    Delete wdd231/scripts directory

--- wdd231/scripts/date.js
+++
@@ -1,6 +0,0 @@
-(() => {
-const yearEl = document.getElementById('year');
-const lastEl = document.getElementById('lastModified');
-if (yearEl) yearEl.textContent = new Date().getFullYear();
-if (lastEl) lastEl.textContent = `Last modified: ${document.lastModified}`;
-})();
